6098c74d4047793cb07c107163007b5a1464ff79
Jeffrey Ricker <jeffrey.ricker@gmail.com>
2018-06-11 08:22:28 -0500

    Changed Collection types from List to IList or IEnumerable depending on usage

--- src/TechJobs/Controllers/ListController.cs
+++ src/TechJobs/Controllers/ListController.cs
@@ -31,14 +31,14 @@ namespace TechJobs.Controllers
         {
             if (column.Equals("all"))
             {
-                List<Dictionary<string, string>> jobs = JobData.FindAll();
+                IList<Dictionary<string, string>> jobs = JobData.FindAll();
                 ViewBag.title =  "All Jobs";
                 ViewBag.jobs = jobs;
                 return View("Jobs");
             }
             else
             {
-                List<string> items = JobData.FindAll(column);
+                IList<string> items = JobData.FindAll(column);
                 ViewBag.title =  "All " + columnChoices[column] + " Values";
                 ViewBag.column = column;
                 ViewBag.items = items;
@@ -48,7 +48,7 @@ namespace TechJobs.Controllers
 
         public IActionResult Jobs(string column, string value)
         {
-            List<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(column, value);
+            IEnumerable<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(column, value);
             ViewBag.title = "Jobs with " + columnChoices[column] + ": " + value;
             ViewBag.jobs = jobs;
 
